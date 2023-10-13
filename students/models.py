28b853a87ae6eb4b80579a116f26d1d4fc31ef77
Nick <nickdevops6@gmail.com>
2023-10-13 05:17:15 +0300

    add abstract user class

--- students/models.py
+++ students/models.py
@@ -1,22 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-SOURCE_CHOICES = (
-    ('YouTube', 'YouTube'),
-    ('Google', 'Google'),
-    ('NewsLetter', 'NewsLetter'),
-)
+
+class User(AbstractUser):
+    pass
 
 
-# Create your models here.
 class Student(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
-
-    paid = models.BooleanField(default=False)
-    source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
-
-    profile_pic = models.ImageField(blank=True, null=True)
-    special_files = models.FileField(blank=True, null=True)
+    teacher = models.ForeignKey("Teacher", on_delete=models.CASCADE)
 
 
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+
+    # paid = models.BooleanField(default=False)
+    # source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
+    #
+    # profile_pic = models.ImageField(blank=True, null=True)
+    # special_files = models.FileField(blank=True, null=True)

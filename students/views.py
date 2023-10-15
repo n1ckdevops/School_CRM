50d0521941bab08a045c4d2158b7a08b1de0187e
Nick <nickdevops6@gmail.com>
2023-10-16 01:56:02 +0300

    add student list, nav and details view

--- students/views.py
+++ students/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Student
+
+
+def student_list(request):
+    students = Student.objects.all()
+    context = {
+        'students': students
+    }
+    return render(request, 'students/student_list.html', context)
+
+
+def student_detail(request, pk):
+    student = Student.objects.get(id=pk)
+    context = {
+        'student': student,
+
+    }
+    return render(request, 'students/student_detail.html', context)

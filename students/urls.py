ae9d6eaa60deb961b0b1e9653a5275c40f61b8b1
Nick <nickdevops6@gmail.com>
2023-10-17 12:51:32 +0300

    add forms and create views

--- students/urls.py
+++ students/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import *
 
+from .views import student_create, student_list, student_detail
 
 app_name = 'students'
 
 urlpatterns = [
     path('', student_list),
-    path('<pk>/', student_detail),
-
-
+    path('<int:pk>/', student_detail),
+    path('create', student_create)
 ]
--- students/views.py
+++ students/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .models import Student
+from .forms import StudentForm
+from .models import Student, Teacher
 
 
 def student_list(request):
@@ -19,3 +19,28 @@ def student_detail(request, pk):
 
     }
     return render(request, 'students/student_detail.html', context)
+
+
+def student_create(request):
+    if request.method == 'POST':
+        print("Receiving a post method")
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            print('the form is valid')
+            print(form.cleaned_data)
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            teacher = Teacher.objects.first()
+            Student.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                age=age,
+                teacher=teacher
+            )
+            print('Student has been created!')
+            return redirect('/students')
+    context = {
+        'form': form
+    }
+    return render(request, 'students/student_create.html', context)

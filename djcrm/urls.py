01c9c56cf56836190d78bbafaef40a2c84d8ec14
Nick <nickdevops6@gmail.com>
2024-01-17 23:51:17 +0300

    add: reset the password

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
 from students.views import LandingPageView, SignupView
 
 
